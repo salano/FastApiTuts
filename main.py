b99cb0901240c311ddd365b89fe4d78e22e935a7
Cleveland Sullivan <salanosullivan@gmail.com>
2024-09-28 22:30:59 +0200

    using Response Model

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from enum import Enum
 from uuid import UUID
 
 from fastapi import FastAPI, Query, Path, Body, Cookie, Header
-from pydantic import BaseModel, Field, HttpUrl
-from typing import Optional, List
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from typing import Optional, List, Literal
 
 app = FastAPI()
 
@@ -321,7 +321,7 @@ async def read_items(
         "start_process": start_process,
         "duration": duration,
     }
-'''
+
 
 ## Part 12 - Cookie and Header Parameters
 @app.get("/items")
@@ -339,5 +339,64 @@ async def read_items(
         "User-Agent": user_agent,
         "X-Token values": x_token,
     }
+'''
 
+
+## Part 13 - Response Model
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: float = 10.5
+    tags: List[str] = []
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: Literal["foo", "bar", "baz"]):
+    return items[item_id]
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    return item
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user: UserIn):
+    return user
+
+
+@app.get(
+    "/items/{item_id}/name",
+    response_model=Item,
+    response_model_include={"name", "description"},
+)
+async def read_item_name(item_id: Literal["foo", "bar", "baz"]):
+    return items[item_id]
+
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+async def read_items_public_data(item_id: Literal["foo", "bar", "baz"]):
+    return items[item_id]
 
