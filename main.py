44f0298e6533d0039f5dbfd9925ab94eee4c99b3
Cleveland Sullivan <salanosullivan@gmail.com>
2024-09-28 23:03:11 +0200

    using Extra Model

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from uuid import UUID
 
 from fastapi import FastAPI, Query, Path, Body, Cookie, Header
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
-from typing import Optional, List, Literal
+from typing import Optional, List, Literal, Union
 
 app = FastAPI()
 
@@ -339,7 +339,7 @@ async def read_items(
         "User-Agent": user_agent,
         "X-Token values": x_token,
     }
-'''
+
 
 
 ## Part 13 - Response Model
@@ -399,4 +399,87 @@ async def read_item_name(item_id: Literal["foo", "bar", "baz"]):
 @app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
 async def read_items_public_data(item_id: Literal["foo", "bar", "baz"]):
     return items[item_id]
+'''
+
+
+## Part 14 - Extra Models
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str):
+    return f"supersecret{raw_password}"
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User 'saved'.")
+    return user_in_db
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
+
+
+class BaseItem(BaseModel):
+    description: str
+    type: str
+
+
+class CarItem(BaseItem):
+    pass
+
+
+class PlaneItem(BaseItem):
+    size: int
+    
+
+items = {
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
+}
+
+
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: Literal["item1", "item2"]):
+    return items[item_id]
+
+
+class ListItem(BaseModel):
+    name: str
+    description: str
+
+
+list_items = [
+    {"name": "Foo", "description": "There comes my hero"},
+    {"name": "Red", "description": "It's my aeroplane"},
+]
+
+
+@app.get("/list_items/", response_model=List[ListItem])
+async def read_items():
+    return items
+
+
 
