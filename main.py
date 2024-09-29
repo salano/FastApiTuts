9fe8ad65c67edeaa2947956af98e0ad6516833f3
Cleveland Sullivan <salanosullivan@gmail.com>
2024-09-29 15:45:37 +0200

    using path operation configuration

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from fastapi import (
     File, 
     UploadFile, 
     HTTPException,
-    Request
+    Request,
+    status
 
 )
 from fastapi.encoders import jsonable_encoder
@@ -24,7 +25,7 @@ from fastapi.exception_handlers import (
 )
 from fastapi.responses import JSONResponse, PlainTextResponse
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
-from typing import Optional, List, Literal, Union
+from typing import Optional, List, Literal, Union, Set
 from starlette.responses import HTMLResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
@@ -565,7 +566,6 @@ async def create_file(
         "fileb_content_type": fileb.content_type,
         "hello": hello,
     }
-'''
 
 
 ## Part 19 - Handling Errors
@@ -655,3 +655,57 @@ async def read_items(item_id: int):
     if item_id == 3:
         raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
     return {"item_id": item_id}
+'''
+
+
+## Part 20 - Path Operation Configuration
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+    tags: Set[str] = []
+
+
+class Tags(Enum):
+    items = "items"
+    users = "users"
+
+
+@app.post(
+    "/items/",
+    response_model=Item,
+    status_code=status.HTTP_201_CREATED,
+    tags=[Tags.items],
+    summary="Create an Item-type item",
+    # description="Create an item with all the information: "
+    # "name; description; price; tax; and a set of "
+    # "unique tags",
+    response_description="The created item",
+)
+async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
+    return item
+
+
+@app.get("/items/", tags=[Tags.items])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=[Tags.users])
+async def read_users():
+    return [{"username": "PhoebeBuffay"}]
+
+
+@app.get("/elements/", tags=[Tags.items], deprecated=True)
+async def read_elements():
+    return [{"item_id": "Foo"}]
