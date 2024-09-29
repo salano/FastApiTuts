b735e1e66b9c2b5878c458ea463e01fb8593acfa
Cleveland Sullivan <salanosullivan@gmail.com>
2024-09-29 17:20:21 +0200

    classes as dependencies

--- main.py
+++ main.py
@@ -761,7 +761,8 @@ def patch_item(item_id: str, item: Item):
     updated_item = stored_item_model.copy(update=update_data)
     items[item_id] = jsonable_encoder(updated_item)
     return updated_item
-'''
+
+
 ## Part 22 - Dependencies Intro
 async def hello():
     return "world"
@@ -781,3 +782,23 @@ async def read_items(commons: dict = Depends(common_parameters)):
 @app.get("/users/")
 async def read_users(commons: dict = Depends(common_parameters)):
     return commons
+'''
+## Part 23 - Classes as Dependencies
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items/{item_id}")
+async def read_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
