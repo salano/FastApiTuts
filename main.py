a10ca0d6b64a95b131d804fc3aa01b361e0cbf43
Cleveland Sullivan <salanosullivan@gmail.com>
2024-09-29 16:12:55 +0200

    using JSON Compatible Encoder and Body Updates

--- main.py
+++ main.py
@@ -655,7 +655,6 @@ async def read_items(item_id: int):
     if item_id == 3:
         raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
     return {"item_id": item_id}
-'''
 
 
 ## Part 20 - Path Operation Configuration
@@ -709,3 +708,56 @@ async def read_users():
 @app.get("/elements/", tags=[Tags.items], deprecated=True)
 async def read_elements():
     return [{"item_id": "Foo"}]
+'''
+
+
+## Part 21a - JSON Compatible Encoder
+class Item(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    tax: float = 10.5
+    tags: List[str] = []
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {
+        "name": "Bar",
+        "description": "The bartenders",
+        "price": 62,
+        "tax": 20.2,
+    },
+    "baz": {
+        "name": "Baz",
+        "description": None,
+        "price": 50.2,
+        "tax": 10.5,
+        "tags": [],
+    },
+}
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items.get(item_id)
+
+
+@app.put("/items/{item_id}", response_model=Item)
+def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
+
+
+@app.patch("/items/{item_id}", response_model=Item)
+def patch_item(item_id: str, item: Item):
+    stored_item_data = items.get(item_id)
+    if stored_item_data is not None:
+        stored_item_model = Item(**stored_item_data)
+    else:
+        stored_item_model = Item()
+    update_data = item.dict(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
